6eaef8fc9f0469879bfe2187cd2daae3de8387f9
RASIN TRIRATTANACHOTE <rasin.tr@gmail.com>
2018-03-26 13:58:48 +0700

    imsi4.0

--- data.php
+++ data.php
@@ -1,17 +1,53 @@
-<?php
-	
-	echo "Data.php";
-	
-<?
-#####################
-## TEST INSERT MAKAIKYKI
-<?php
+<ul class="nav nav-tabs" id="tabs">
+        <li class="active">
+            <a href="#language_internal_1" data-toggle="tab">
+                 Language 1 
+            </a>
+        </li>
+        <li id="select_language_internal_2_display" style="display: none;">
+            <a href="#language_internal_2" data-toggle="tab">
+                 Language 2 
+            </a>
+        </li>
+        <li id="select_language_internal_3_display">
+            <a href="#language_internal_3" data-toggle="tab">
+                 Language 3 
+            </a>
+        </li>
+    </ul>
 
-	echo "Data.php";
-
-?>
-<?php
-
-	echo "Branch Data Insert";
-
-?>
+    <div class="tab-content">
+        <div class="tab-pane active" id="language_internal_1">
+            <!-- Language 1 -->
+            <div class="form-group">
+                <label class="control-label">
+                	Tab Name (Language 1)
+                </label>
+                <div>
+                    <input type="text" name="internal_tab_name" />
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane fade" id="language_internal_2">
+            <!-- Language 2 -->
+            <div class="form-group">
+                <label class="control-label">
+                	Tab Name (Language 2)
+                </label>
+                <div>
+                    <input type="text" name="internal_tab_name_2" />
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane fade" id="language_internal_3">
+            <!-- Language 3 -->
+            <div class="form-group">
+                <label class="control-label">
+                	Tab Name (Language 3)
+                </label>
+                <div>
+                    <input type="text" name="internal_tab_name_3" />
+                </div>
+            </div>
+        </div>
+    </div>
